d6244579d65ca10f56892afedd3e88f1258e0102
Jack Baron <jackmbaron@gmail.com>
2022-05-23 03:21:54 +0100

    skeleton bot

--- src/env.ts
+++ src/env.ts
@@ -6,3 +6,8 @@ const NODE_ENV = registerString('NODE_ENV')
 const IS_PROD = NODE_ENV?.toLowerCase() === 'production'
 export const IS_DEV = !IS_PROD
 // #endregion
+
+// #region Bot
+export const TOKEN = registerString('TOKEN', true)
+export const GUILD_ID = registerString('GUILD_ID', true)
+// #endregion
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,49 @@
 import 'source-map-support/register.js'
 import 'reflect-metadata'
+
+import { dirname, importx } from '@discordx/importer'
+import { exitHook } from '@lolpants/exit'
+import { Intents } from 'discord.js'
+import { Client } from 'discordx'
+import { join as joinPath } from 'node:path/posix'
+import { GUILD_ID, TOKEN } from '~/env.js'
+
+const client = new Client({
+  silent: true,
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS,
+  ],
+  botGuilds: [GUILD_ID],
+})
+
+client.once('ready', async () => {
+  await client.guilds.fetch()
+  await client.initApplicationCommands()
+})
+
+client.on('interactionCreate', interaction => {
+  void client.executeInteraction(interaction)
+})
+
+const run = async () => {
+  const imports = joinPath(
+    dirname(import.meta.url).replaceAll('\\', '/'),
+    '/{handlers,commands}/**/*.{ts,js}'
+  )
+
+  await importx(imports)
+  await client.login(TOKEN)
+}
+
+exitHook(async (exit, error) => {
+  if (error instanceof Error) {
+    console.error(error)
+  }
+
+  client.destroy()
+  exit()
+})
+
+void run()
