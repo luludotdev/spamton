ec5e7a20edf5321d8e64072ff9f3a9bbb5dd5dfd
Jack Baron <jackmbaron@gmail.com>
2022-05-23 03:41:29 +0100

    add permission checks to pin

--- src/commands/pin.ts
+++ src/commands/pin.ts
@@ -1,10 +1,44 @@
-import { type MessageContextMenuInteraction } from 'discord.js'
+import {
+  DMChannel,
+  GuildMember,
+  type MessageContextMenuInteraction,
+} from 'discord.js'
 import { ContextMenu, Discord } from 'discordx'
 
 @Discord()
 export abstract class Pin {
   @ContextMenu('MESSAGE', 'Pin / Unpin')
   public async messageHandler(interaction: MessageContextMenuInteraction) {
+    const channel = interaction.channel!
+    if (channel instanceof DMChannel || channel.partial) {
+      await interaction.reply({
+        content: 'This cannot be run in DMs!',
+        ephemeral: true,
+      })
+
+      return
+    }
+
+    const member = interaction.member!
+    if (!(member instanceof GuildMember)) {
+      await interaction.reply({
+        content: 'Failed to resolve interaction member!',
+        ephemeral: true,
+      })
+
+      return
+    }
+
+    const permissions = member.permissionsIn(channel)
+    if (!permissions.has('VIEW_CHANNEL') || !permissions.has('SEND_MESSAGES')) {
+      await interaction.reply({
+        content: 'You do not have permission to manage pins in this channel!',
+        ephemeral: true,
+      })
+
+      return
+    }
+
     const message = await interaction.channel?.messages.fetch(
       interaction.targetMessage.id
     )
