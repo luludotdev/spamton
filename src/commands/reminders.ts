5c1caf441a63f3c74d7f87ea34819f7acdb194b7
Jack Baron <jackmbaron@gmail.com>
2024-11-10 03:52:37 +0000

    disable reminders command for now

--- src/commands/reminders.ts
+++ src/commands/reminders.ts
@@ -1,78 +1,72 @@
-import {
-  Attachment,
-  AutocompleteInteraction,
-  CommandInteraction,
-} from "discord.js";
-import { ApplicationCommandOptionType as OptionType } from "discord.js";
-import {
-  Discord,
-  Guild,
-  Slash,
-  SlashOption,
-  SlashGroup,
-  SlashChoice,
-} from "discordx";
-import { env } from "~/env";
-import {
-  commandContext as ctxField,
-  errorField,
-  logger,
-  userField,
-} from "~/logger.js";
+export {};
 
-const context = ctxField("reminders");
+// import {
+//   AutocompleteInteraction,
+//   CommandInteraction,
+//   ApplicationCommandOptionType as OptionType,
+// } from "discord.js";
+// import { Discord, Slash, SlashGroup, SlashOption } from "discordx";
+// import {
+//   commandContext as ctxField,
+//   errorField,
+//   logger,
+//   userField,
+// } from "~/logger.js";
 
-@Discord()
-@SlashGroup({ name: "reminders", description: "Manage reminders" })
-export abstract class Reminders {
-  @Slash({
-    name: "remindme",
-    description: "Set a reminder. Requires DMs to be enabled.",
-  })
-  public async remindme(
-    @SlashOption({
-      name: "time",
-      type: OptionType.String,
-      description:
-        "The relative (5 hours) or absolute (5:00PM) UTC time to set for the reminder",
-      required: true,
-    })
-    time: string,
-    @SlashOption({
-      name: "reminder",
-      type: OptionType.String,
-      description: "The reminder to send",
-      required: true,
-    })
-    reminder: string,
-    ctx: CommandInteraction,
-  ) {
-    // TODO
-  }
+// const context = ctxField("reminders");
 
-  @SlashGroup("reminders")
-  @Slash({ name: "list", description: "Show a list of all pending reminders" })
-  public async listReminders(ctx: CommandInteraction) {
-    // TODO
-  }
+// type ReminderStore =
 
-  @SlashGroup("reminders")
-  @Slash({ name: "delete", description: "Deletes a pending reminder" })
-  public async deleteReminder(
-    @SlashOption({
-      name: "reminder",
-      type: OptionType.String,
-      description: 'The ID of the reminder from "/reminders list"',
-      required: true,
-      autocomplete: true,
-    })
-    id: string,
-    ctx: CommandInteraction | AutocompleteInteraction,
-  ) {
-    if (ctx instanceof AutocompleteInteraction) {
-      // TODO
-      return;
-    }
-    // TODO
-  }
-}
+// @Discord()
+// @SlashGroup({ name: "reminders", description: "Manage reminders" })
+// export abstract class Reminders {
+//   @Slash({
+//     name: "remindme",
+//     description: "Set a reminder. Requires DMs to be enabled.",
+//   })
+//   public async remindme(
+//     @SlashOption({
+//       name: "time",
+//       type: OptionType.String,
+//       description:
+//         "The relative (5 hours) or absolute (5:00PM) UTC time to set for the reminder",
+//       required: true,
+//     })
+//     time: string,
+//     @SlashOption({
+//       name: "reminder",
+//       type: OptionType.String,
+//       description: "The reminder to send",
+//       required: true,
+//     })
+//     reminder: string,
+//     ctx: CommandInteraction,
+//   ) {
+//     // TODO
+//   }
+
+//   @SlashGroup("reminders")
+//   @Slash({ name: "list", description: "Show a list of all pending reminders" })
+//   public async listReminders(ctx: CommandInteraction) {
+//     // TODO
+//   }
+
+//   @SlashGroup("reminders")
+//   @Slash({ name: "delete", description: "Deletes a pending reminder" })
+//   public async deleteReminder(
+//     @SlashOption({
+//       name: "reminder",
+//       type: OptionType.String,
+//       description: 'The ID of the reminder from "/reminders list"',
+//       required: true,
+//       autocomplete: true,
+//     })
+//     id: string,
+//     ctx: AutocompleteInteraction | CommandInteraction,
+//   ) {
+//     if (ctx instanceof AutocompleteInteraction) {
+//       // TODO
+//     }
+//     // TODO
+//   }
+// }
