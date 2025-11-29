51641476159597ab4c532904ec4b7abf0c2f37d4
Jack Baron <jackmbaron@gmail.com>
2025-11-29 22:18:41 +0000

    dont need this anymore

--- src/commands/pin.ts
+++ src/commands/pin.ts
@@ -1,92 +1,94 @@
-import type { MessageContextMenuCommandInteraction as MessageContextMenuInteraction } from "discord.js";
-import { ApplicationCommandType, GuildMember } from "discord.js";
-import { ContextMenu, Discord, Guild } from "discordx";
-import { env } from "~/env";
-import {
-  channelField,
-  commandContext as ctxField,
-  errorField,
-  logger,
-  userField,
-} from "~/logger.js";
+export {};
 
-const context = ctxField("pin");
+// import type { MessageContextMenuCommandInteraction as MessageContextMenuInteraction } from "discord.js";
+// import { ApplicationCommandType, GuildMember } from "discord.js";
+// import { ContextMenu, Discord, Guild } from "discordx";
+// import { env } from "~/env";
+// import {
+//   channelField,
+//   commandContext as ctxField,
+//   errorField,
+//   logger,
+//   userField,
+// } from "~/logger.js";
 
-@Discord()
-export abstract class Pin {
-  @Guild(env.GUILD_ID)
-  @ContextMenu({ name: "Pin / Unpin", type: ApplicationCommandType.Message })
-  public async messageHandler(interaction: MessageContextMenuInteraction) {
-    const channel = interaction.channel!;
-    if (channel.isDMBased()) {
-      await interaction.reply({
-        content: "This cannot be run in DMs!",
-        ephemeral: true,
-      });
+// const context = ctxField("pin");
 
-      return;
-    }
+// @Discord()
+// export abstract class Pin {
+//   @Guild(env.GUILD_ID)
+//   @ContextMenu({ name: "Pin / Unpin", type: ApplicationCommandType.Message })
+//   public async messageHandler(interaction: MessageContextMenuInteraction) {
+//     const channel = interaction.channel!;
+//     if (channel.isDMBased()) {
+//       await interaction.reply({
+//         content: "This cannot be run in DMs!",
+//         ephemeral: true,
+//       });
 
-    const member = interaction.member!;
-    if (!(member instanceof GuildMember)) {
-      await interaction.reply({
-        content: "Failed to resolve interaction member!",
-        ephemeral: true,
-      });
+//       return;
+//     }
 
-      return;
-    }
+//     const member = interaction.member!;
+//     if (!(member instanceof GuildMember)) {
+//       await interaction.reply({
+//         content: "Failed to resolve interaction member!",
+//         ephemeral: true,
+//       });
 
-    const permissions = member.permissionsIn(channel);
-    if (!permissions.has("ViewChannel") || !permissions.has("SendMessages")) {
-      await interaction.reply({
-        content: "You do not have permission to manage pins in this channel!",
-        ephemeral: true,
-      });
+//       return;
+//     }
 
-      return;
-    }
+//     const permissions = member.permissionsIn(channel);
+//     if (!permissions.has("ViewChannel") || !permissions.has("SendMessages")) {
+//       await interaction.reply({
+//         content: "You do not have permission to manage pins in this channel!",
+//         ephemeral: true,
+//       });
 
-    const message = await interaction.channel?.messages.fetch(
-      interaction.targetMessage.id,
-    );
+//       return;
+//     }
 
-    if (message === undefined) {
-      await interaction.reply({
-        content: "Failed to find message!",
-        ephemeral: true,
-      });
+//     const message = await interaction.channel?.messages.fetch(
+//       interaction.targetMessage.id,
+//     );
 
-      return;
-    }
+//     if (message === undefined) {
+//       await interaction.reply({
+//         content: "Failed to find message!",
+//         ephemeral: true,
+//       });
 
-    const isPinned = message.pinned;
-    try {
-      const action = isPinned ? "unpinned" : "pinned";
+//       return;
+//     }
 
-      await (isPinned ? message.unpin() : message.pin());
-      await interaction.reply({ content: `Message ${action}.` });
+//     const isPinned = message.pinned;
+//     try {
+//       const action = isPinned ? "unpinned" : "pinned";
 
-      logger.info({
-        ...context,
-        action,
-        user: userField(interaction.user),
-        channel: channelField(channel),
-        message: message.id,
-      });
-    } catch (error: unknown) {
-      const action = isPinned ? "unpin" : "pin";
-      let message = `Failed to ${action} message!`;
+//       await (isPinned ? message.unpin() : message.pin());
+//       await interaction.reply({ content: `Message ${action}.` });
 
-      if (error instanceof Error) {
-        logger.error({ ...context, ...errorField(error) });
-        message += `\n${error.message}`;
-      }
+//       logger.info({
+//         ...context,
+//         action,
+//         user: userField(interaction.user),
+//         channel: channelField(channel),
+//         message: message.id,
+//       });
+//     } catch (error: unknown) {
+//       const action = isPinned ? "unpin" : "pin";
+//       let message = `Failed to ${action} message!`;
 
-      await interaction.reply({
-        content: message,
-        ephemeral: true,
-      });
-    }
-  }
-}
+//       if (error instanceof Error) {
+//         logger.error({ ...context, ...errorField(error) });
+//         message += `\n${error.message}`;
+//       }
+
+//       await interaction.reply({
+//         content: message,
+//         ephemeral: true,
+//       });
+//     }
+//   }
+// }
